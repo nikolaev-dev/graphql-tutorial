a17e53e95c10d56297d8a4b42d1ce0982626f4bb
Alexander Nikolaev <ni-ko-la@mail.ru>
2018-08-23 18:09:38 +0300

    init commit and add graphql

---
+++ app/graphql/types/link_type.rb
@@ -0,0 +1,9 @@
+Types::LinkType = GraphQL::ObjectType.define do
+  # this type is named `Link`
+  name 'Link'
+
+  # it has the following fields
+  field :id, !types.ID
+  field :url, !types.String
+  field :description, !types.String
+end
